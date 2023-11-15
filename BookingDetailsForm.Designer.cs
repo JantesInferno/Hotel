49675e27048b62e25f5e04e8f3b040ff0989c935
JantesInferno <123421286+JantesInferno@users.noreply.github.com>
2023-11-15 16:15:28 +0100

    c

--- BookingDetailsForm.Designer.cs
+++ BookingDetailsForm.Designer.cs
@@ -486,7 +486,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.Color.Brown;
             this.ClientSize = new System.Drawing.Size(1036, 340);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel3);
